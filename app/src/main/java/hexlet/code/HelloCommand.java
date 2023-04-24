58669b9103a0985e2617ac85f17d6a97ffa75b2e
mudrec <sm1le88man@gmail.com>
2023-04-24 11:25:10 +0300

    update HelloCommand, add -f and filepath params

--- app/src/main/java/hexlet/code/HelloCommand.java
+++ app/src/main/java/hexlet/code/HelloCommand.java
@@ -1,6 +1,8 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @CommandLine.Command(
         name = "hello",
@@ -8,6 +10,22 @@ import picocli.CommandLine;
         mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class HelloCommand implements Runnable {
+    @Option(
+            names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            defaultValue = "stylish"
+    )
+    private String format;
+
+    @Parameters(
+            paramLabel = "filepath1",
+            description = "path to first file")
+    private String filepath1;
+    @Parameters(
+            paramLabel = "filepath2",
+            description = "path to second file")
+    private String filepath2;
+
     @Override
     public void run() {
         System.out.println("Hello World!");

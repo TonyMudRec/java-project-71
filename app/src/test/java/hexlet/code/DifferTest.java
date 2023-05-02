6302005d673ef40d526977836fcdf5f10b877e0e
mudrec <sm1le88man@gmail.com>
2023-05-02 15:50:10 +0300

    add resourses and tests

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,35 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DifferTest {
+
+    @Test
+    void generateTest() throws IOException {
+        String testFilePath1 = "src/test/resources/files/file1.json";
+        String testFilePath2 = "src/test/resources/files/file2.json";
+        String expected = "{\n"
+                + " - follow: false\n"
+                + "   host: hexlet.io\n"
+                + " - proxy: 123.234.53.22\n"
+                + " - timeout: 50\n"
+                + " + timeout: 20\n"
+                + " + verbose: true\n"
+                + "}";
+
+        assertThat(Differ.generate(testFilePath1, testFilePath2)).isEqualTo(expected);
+    }
+
+    @Test
+    void checkExistFile() throws IOException {
+        String testFilePath1 = "src/test/resources/files/file1.json";
+        String testFilePath2 = "src/test/resources/files/file3.json";
+
+        assertThat(Differ.convertPathToAbsolutePath(testFilePath1)).isNotNull();
+        assertThat(Differ.convertPathToAbsolutePath(testFilePath2)).isNull();
+    }
+}

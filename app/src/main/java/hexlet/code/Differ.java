f76f86c86b306b2568f32168895bf80667961290
mudrec <sm1le88man@gmail.com>
2023-05-16 16:51:32 +0300

    method generate overloaded

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -23,4 +23,18 @@ public class Differ {
         return Formatter.useForm(format, mapFile1, mapFile2, jointMap);
     }
 
+    public static String generate(String filepath1, String filepath2) {
+        Map<String, Object> mapFile1 = new HashMap<>();
+        Map<String, Object> mapFile2;
+        try {
+            mapFile1 = Parser.parse(filepath1);
+            mapFile2 = Parser.parse(filepath2);
+        } catch (NullPointerException | IOException e) {
+            String filepath = mapFile1.isEmpty() ? filepath1 : filepath2;
+            return "File '" + filepath + "' does not exist, or cannot be reading";
+        }
+        Map<String, Object> jointMap = new TreeMap<>(mapFile1);
+        jointMap.putAll(mapFile2);
+        return Formatter.useForm("stylish", mapFile1, mapFile2, jointMap);
+    }
 }

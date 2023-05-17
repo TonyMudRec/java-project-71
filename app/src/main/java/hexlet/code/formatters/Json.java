7c2fa6badb979724112e081897329aa162b3c21e
mudrec <sm1le88man@gmail.com>
2023-05-17 15:40:15 +0300

    try to fix some errors

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,33 +1,34 @@
 package hexlet.code.formatters;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Json {
     public static String construct(Map<String, Object> mapFile1, Map<String, Object> mapFile2, Map<String,
             Object> jointMap) {
+        String currentValue;
+        String lastValue;
         StringBuilder sb = new StringBuilder();
-        sb.append("{" + "\n");
+        sb.append("{");
         for (String key : jointMap.keySet()) {
-            String currentValue = transformToString(jointMap.get(key));
-            String lastValue = transformToString(mapFile1.get(key));
-            if (!mapFile1.containsKey(key) || (mapFile2.containsKey(key) && !lastValue.equals(currentValue))) {
-                sb.append("    \"").append(key).append("\": ").append(currentValue).append(",\n");
+            currentValue = jointMap.get(key) == null ? "null" : jointMap.get(key).toString();
+            if (mapFile1.containsKey(key) && mapFile2.containsKey(key)) {
+                lastValue = mapFile1.get(key) == null ? "null" : mapFile1.get(key).toString();
+                if (!lastValue.equals(currentValue)) {
+                    sb.append("\"- ").append(key).append("\":\"").append(lastValue).append("\",");
+                    sb.append("\"+ ").append(key).append("\":\"").append(currentValue).append("\",");
+                } else {
+                    sb.append("\"").append(key).append("\":\"").append(currentValue).append("\",");
+                }
+            } else if (!mapFile1.containsKey(key)) {
+                sb.append("\"+ ").append(key).append("\":\"").append(currentValue).append("\",");
+            } else {
+                lastValue = mapFile1.get(key) == null ? "null" : mapFile1.get(key).toString();
+                sb.append("\"- ").append(key).append("\":\"").append(lastValue).append("\",");
             }
         }
         sb.setLength(sb.length() - 1);
-        sb.append("\n}");
+        sb.append("}");
         return sb.toString();
-    }
-
-    public static String transformToString(Object o) {
-        String result;
-        if (o == null) {
-            result = "null";
-        } else if (o instanceof String) {
-            result = "\"" + o + "\"";
-        } else {
-            result = o.toString();
-        }
-        return result;
     }
 }

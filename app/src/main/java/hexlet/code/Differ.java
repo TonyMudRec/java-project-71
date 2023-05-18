84218958e49f38cada06ce6859cddd6f8b8d3a40
mudrec <sm1le88man@gmail.com>
2023-05-18 18:09:13 +0300

    polish

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,15 +7,28 @@ import java.util.Map;
 public class Differ {
 
     public static String generate(String filepath1, String filepath2, String format) {
-        Map<String, Object> mapFile1 = new HashMap<>();
-        Map<String, Object> mapFile2;
-        try {
-            mapFile1 = Parser.parse(filepath1);
-            mapFile2 = Parser.parse(filepath2);
-        } catch (NullPointerException | IOException e) {
-            String filepath = mapFile1.isEmpty() ? filepath1 : filepath2;
-            return "File '" + filepath + "' does not exist, or cannot be reading";
+        Map<String, Object> mapFile1 = parser(filepath1);
+        if (mapFile1.isEmpty()) {
+            return "File '" + filepath1 + "' does not exist, or cannot be reading";
+        }
+        Map<String, Object> mapFile2 = parser(filepath2);
+        if (mapFile1.isEmpty()) {
+            return "File '" + filepath2 + "' does not exist, or cannot be reading";
         }
         return Formatter.useForm(format, mapFile1, mapFile2);
+    }
+
+    public static String generate(String filepath1, String filepath2) {
+        return generate(filepath1, filepath2, "stylish");
+    }
+
+    public static Map<String, Object> parser(String filepath) {
+        Map<String, Object> mapFile;
+        try {
+            mapFile = Parser.parse(filepath);
+        } catch (NullPointerException | IOException e) {
+            return new HashMap<>();
+        }
+        return mapFile;
     }
 }
